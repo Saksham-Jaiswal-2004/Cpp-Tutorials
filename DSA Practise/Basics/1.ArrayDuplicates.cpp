601b14607e5a4daa25f6f63af7065efd9703161e
Saksham Jaiswal <sakshamjaiswalofficial@gmail.com>
2025-03-15 11:11:25 +0530

    Leetcode

--- DSA Practise/Basics/1.ArrayDuplicates.cpp
+++ DSA Practise/Basics/1.ArrayDuplicates.cpp
@@ -5,39 +5,32 @@ using namespace std;
 
 int removeDuplicates(vector<int>& nums) 
 {
-    int len = nums.size();
-    int count = 0;
+    if (nums.empty()) return 0;  // Edge case: empty array
 
-    for(int i=0 ; i<len-1 ; i++)
-    {
-        if(nums[i] != nums[i+1])
-        {
-            count++;
-            cout<<nums[i]<<" : "<<nums[i+1]<<endl;
-        }
-        else
-        {
-            for(int j=i ; j<len ; j++)
-            {
-                nums[j] = nums[j+1];
-            }
-            i--;
+    int j = 0;  // Slow pointer for unique elements
+    for (int i = 1; i < nums.size(); i++) {
+        if (nums[i] != nums[j]) {  // Found a new unique element
+            j++;
+            nums[j] = nums[i];  // Move unique element forward
         }
     }
 
-    for(int i: nums)
-    {
-        cout<<i<<" ";
-    }
-    cout<<endl;
-
-    return count+1;
+    return j + 1;  // Number of unique elements
 }
 
 int main()
 {
-    vector<int> data = {0,0,0,1,1,2,3,4,5,5};
+    vector<int> data = {-2,-1,0,0,1,1,1,2,2,3,3,4};
+    // vector<int> data = {1,2,2};
+    // vector<int> data = {1,1};
+    // vector<int> data = {};
 
-    cout<<"Number of unique elements: "<<removeDuplicates(data)<<endl;
+    int a = removeDuplicates(data);
+    cout<<"Number of unique elements: "<<a<<endl;
+
+    for(int i=0 ; i<a ; i++)
+    {
+        cout<<data[i]<<" ";
+    }
     return 0;
 }
