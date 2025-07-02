123a001bbd42cdd5561b69d95d1c863a63374daf
Saksham Jaiswal <sakshamjaiswalofficial@gmail.com>
2025-07-03 00:02:25 +0530

    DSA - Stack

--- DSA/Stack/46.Stack.cpp
+++ DSA/Stack/46.Stack.cpp
@@ -5,5 +5,12 @@ using namespace std;
 
 int main()
 {
+    stack<int> s;
+    s.push(3);
+    s.push(5);
+
+    cout<<"Top: "<<s.top()<<endl;
+    cout<<"Empty: "<<s.empty()<<endl;
+
     return 0;
 }
